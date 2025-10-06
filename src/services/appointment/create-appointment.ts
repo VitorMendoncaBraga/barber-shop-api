88448f77ea958db48ec937245c4f22b8b760f4a1
Vitor Mendonça Braga <vmbbraga5@gmail.com>
2025-10-06 17:58:09 -0300

    feat: create appointment service

--- src/services/appointment/create-appointment.ts
+++ src/services/appointment/create-appointment.ts
@@ -1,3 +1,27 @@
+import { Appointment } from "../../generated/prisma";
+import { AppointmentRepository } from "../../repositories/appointment-repository";
+
+interface CreateAppointmentServiceRequest{
+    userId: string,
+    barberId: string,
+    date: Date,
+}
+
+interface CreateAppointmentServiceResponse{
+   appointment: Appointment
+}
+
 export class CreateAppointmentService {
-    
+    private appointmentsRepository: AppointmentRepository
+
+    constructor(appointmentsRepository: AppointmentRepository){
+        this.appointmentsRepository = appointmentsRepository
+    }
+
+    async execute({barberId,date,userId} : CreateAppointmentServiceRequest): Promise<CreateAppointmentServiceResponse>{
+        const appointment = await this.appointmentsRepository.create({barberId,date,userId})
+        return {
+            appointment
+        }
+    }
 }
